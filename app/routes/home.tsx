d5e6b87008a4e0b8b25534dcd15a9b424edc1d5e
Azfar <azfarrazzaq23@gmail.com>
2025-05-22 12:11:54 +0500

    Add carousel component with animated marquee for logos and data.json

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import logosLinks from "~/data.json";
+import type { Route } from "+/types/home";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export async function loader() {
+  const logos = logosLinks;
+  return { logos };
 }
 
-export default function Home() {
-  return <Welcome />;
+export default function Carousel({ loaderData }: Route.LoaderArgs) {
+  const { logos } = loaderData;
+
+  return (
+    <div className=" w-full mt-12 relative flex overflow-x-hidden bg-scrollerColor">
+      <div className="animate-marquee py-6 flex  whitespace-nowrap flex-shrink-0">
+        {logos.map((logo, index) => (
+          <img
+            key={`A${index}`}
+            src={logo.link}
+            className="h-12 w-12 md:h-16 md:w-16   rounded-full bg-gray-200 mx-4"
+            alt="Coin Logo"
+          />
+        ))}
+      </div>
+      <div className="animate-marquee top-0 py-6 flex whitespace-nowrap flex-shrink-0">
+        {logos.map((logo, index) => (
+          <img
+            key={`B${index}`}
+            src={logo.link}
+            className="h-12 w-12 md:h-16 md:w-16   rounded-full bg-gray-200 mx-4"
+            alt="Coin Logo"
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
